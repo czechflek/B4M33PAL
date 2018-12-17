e88198a1de4523ddba4dd8f254365489bd7301f1
Jakub Čech <contact@kubacech.cz>
2018-12-17 18:12:49 +0100

    No map

--- 5-StringFactors/src/pal/Trie.java
+++ 5-StringFactors/src/pal/Trie.java
@@ -9,10 +9,17 @@ public class Trie {
 
     void insertWord(String word, int wordId) {
         TrieNode currentNode = root;
+        TrieNode next;
         for (int i = 0; i < word.length(); i++) {
-            currentNode = currentNode.getChildren().computeIfAbsent(word.charAt(i), k -> new TrieNode());
-            if(currentNode.isWord())
-                return;
+            next = currentNode.getChild(word.charAt(i));
+            if (next == null) {
+                currentNode = currentNode.setChild(word.charAt(i));
+            } else {
+                currentNode = next;
+                if (currentNode.isWord())
+                    return;
+            }
+
         }
         currentNode.setWordId(wordId);
     }
@@ -20,7 +27,7 @@ public class Trie {
     Result search(int offset) {
         TrieNode currentNode = root;
         for (int i = offset; i < sequence.length(); i++) {
-            currentNode = currentNode.getChildren().get(sequence.charAt(i));
+            currentNode = currentNode.getChild(sequence.charAt(i));;
             if (currentNode == null) {
                 return null;
             } else if (currentNode.isWord()) {
@@ -47,19 +54,22 @@ public class Trie {
     private class TrieNode {
         private static final int NOT_WORD = -1;
 
-        private Map<Character, TrieNode> children = new HashMap<>();
+        //private Map<Character, TrieNode> children = new HashMap<>();
+        private TrieNode[] children = new TrieNode[26];
         int wordId = NOT_WORD;
 
-        Map<Character, TrieNode> getChildren() {
-            return children;
+        TrieNode getChild(char c) {
+            return children[c - 'a'];
+        }
+
+        TrieNode setChild(char c) {
+            TrieNode node = new TrieNode();
+            children[c - 'a'] = node;
+            return node;
         }
 
         boolean isWord() {
             return wordId > NOT_WORD;
-        }
-
-        int getWordId() {
-            return wordId;
         }
 
         void setWordId(int wordId) {
